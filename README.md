de7a3933f8302ddc1c14dcd366f904f3b08e2a4e
Guxta556 <169097896+Guxta556@users.noreply.github.com>
2024-05-06 18:03:51 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/GUSTAVO-PIZZATO/pen/PogMvqM](https://codepen.io/GUSTAVO-PIZZATO/pen/PogMvqM).
+
---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("simButton").addEventListener("click", function () {
+  var newPageContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Nova Página</title>
+      <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+      <div class="container">
+        <h1>Oiii Bianca, minha NAMORADA</h1>
+        <h2>Saiba que eu te amo muito, você é muito importante para mim, obrigado por ser um presente na minha vida, obrigado por todos os momento juntos !<h2>
+        <p>EU TE AMO MUITOOOO !!!</p>
+        <h1>Ta caga tronco me valoriza ta, isso deu um trabalho da gota!/div>
+      </div>
+    </body>
+    </html>
+  `;
+  
+  document.write(newPageContent);
+});
